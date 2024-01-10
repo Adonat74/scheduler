74e7d01ed1593497c9106e1e393aababec6b466a
Adonat74 <donatgoninet.antoine@gmail.com>
2024-01-10 18:38:41 +0100

    grid rendue interactive

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -24,12 +24,10 @@ for (let i = 0; i < 216; i++) {
 document.querySelector(".grid-container").innerHTML = gridElement;
 
 
-
-
 let catElement = "";
 
 categories.forEach((cat) => {
-    catElement += `<div class="cat-element">
+    catElement += `<div class="cat-element" data-color="${cat.color}" data-cat="${cat.category}">
         <div class="color ${cat.color}"></div>
         <p>${cat.category}</p>
     </div>`
@@ -38,11 +36,28 @@ document.querySelector(".cat-container").innerHTML = catElement;
 
 
 
+let selectedCat = {
+    category: "delete", color: "white"
+};
+
+document.querySelectorAll(".cat-element").forEach((element) => {
+    element.addEventListener("click", () => {
+        selectedCat.category = element.dataset.cat;
+        selectedCat.color = element.dataset.color;
+    });
+});
+
 
 
 document.querySelectorAll(".grid-element").forEach((element) => {
     element.addEventListener("click", () => {
-        element.classList.add("red");
-        element.innerHTML = "work"
+        if (selectedCat.category === "delete") {
+            element.classList.add(selectedCat.color);
+            element.innerHTML = "";
+        } else {
+            element.classList.add(selectedCat.color);
+            element.innerHTML = selectedCat.category;
+        }
+        
     });
 });
