33839060a31313f76a3b76247d365285223194f1
Adonat74 <donatgoninet.antoine@gmail.com>
2024-01-15 18:11:49 +0100

    cleaning of my code and directory/ remove menu and dev tools

--- main.js
+++ main.js
@@ -1,42 +1,22 @@
 const path = require('path');
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow} = require('electron');
 
-
-const isDev = process.env.NODE_ENV !== 'production';
-const isMac = process.platform === 'darwin';
 
 
 // Main Window
 function createMainWindow() {
     mainWindow = new BrowserWindow({
-      width: isDev ? 1000 : 800,
+      autoHideMenuBar: true,
+      width: 800,
       height: 1000,
-      resizable: isDev,
       webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true
       },
     });
   
-    // Show devtools automatically if in development
-    if (isDev) {
-      mainWindow.webContents.openDevTools();
-    }
   
       // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
      mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
-}
-
-// About Window
-function createAboutWindow() {
-    aboutWindow = new BrowserWindow({
-        width: 300,
-        height: 300,
-        title: 'About Electron'
-    });
-  
-    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
 }
 
 
@@ -44,71 +24,10 @@ function createAboutWindow() {
 app.on('ready', () => {
     createMainWindow();
   
-    const mainMenu = Menu.buildFromTemplate(menu);
-    Menu.setApplicationMenu(mainMenu);
   
     // Remove variable from memory
     mainWindow.on('closed', () => (mainWindow = null));
 });
-
-
-
-// Menu template
-const menu = [
-    ...(isMac
-      ? [
-          {
-            label: app.name,
-            submenu: [
-              {
-                label: 'About',
-                click: createAboutWindow,
-              },
-            ],
-          },
-        ]
-      : []),
-    {
-      role: 'fileMenu',
-    },
-    ...(!isMac
-      ? [
-          {
-            label: 'Help',
-            submenu: [
-              {
-                label: 'About',
-                click: createAboutWindow,
-              },
-            ],
-          },
-        ]
-      : []),
-    // {
-    //   label: 'File',
-    //   submenu: [
-    //     {
-    //       label: 'Quit',
-    //       click: () => app.quit(),
-    //       accelerator: 'CmdOrCtrl+W',
-    //     },
-    //   ],
-    // },
-    ...(isDev
-      ? [
-          {
-            label: 'Developer',
-            submenu: [
-              { role: 'reload' },
-              { role: 'forcereload' },
-              { type: 'separator' },
-              { role: 'toggledevtools' },
-            ],
-          },
-        ]
-      : []),
-  ];
-
 
 
 // Quit when all windows are closed.
@@ -117,7 +36,3 @@ app.on('window-all-closed', () => {
 });
 
 
-// Open a window if none are open (macOS)
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
-});
